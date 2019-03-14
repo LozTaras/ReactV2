fb6ec727b5c4a3322231081703f5a5069d4fa840
Taras Lozinskiy <t.lozinskiy@astoundcommerce.com>
2019-03-14 13:19:38 +0200

    prod

---
+++ precache-manifest.df72e932764dc88e6862588d0f65f04a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "54c4850b1ec35221278c",
+    "url": "/static/js/main.54c4850b.chunk.js"
+  },
+  {
+    "revision": "e6c8a6c3b4cdfc61e868",
+    "url": "/static/js/2.e6c8a6c3.chunk.js"
+  },
+  {
+    "revision": "54c4850b1ec35221278c",
+    "url": "/static/css/main.eb80b6d9.chunk.css"
+  },
+  {
+    "revision": "8cebcdedd905ccfdada8aec4067a6e5b",
+    "url": "/index.html"
+  }
+];
--- src/components/error-boundry/index.js
+++
@@ -1,3 +0,0 @@
-import ErrorIndicator from './error-boundry';
-
-export default ErrorIndicator;
