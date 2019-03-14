d8bd87d28fa37d3446eec351bee2ae38b27ee3d9
Taras Lozinskiy <t.lozinskiy@astoundcommerce.com>
2019-03-14 13:37:29 +0200

    add prod

---
+++ docs/precache-manifest.2f4e236c0f8c9d5a61ff2ea8167a6a83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "9f2866024369453ddd29",
+    "url": "/static/js/main.9f286602.chunk.js"
+  },
+  {
+    "revision": "e6c8a6c3b4cdfc61e868",
+    "url": "/static/js/2.e6c8a6c3.chunk.js"
+  },
+  {
+    "revision": "9f2866024369453ddd29",
+    "url": "/static/css/main.7f0336c8.chunk.css"
+  },
+  {
+    "revision": "50884e733b1d675e72a7bb305cd4c730",
+    "url": "/index.html"
+  }
+];
